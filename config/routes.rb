16ca418a0559d2eb30b8a18aa94c9486e06b9d71
Flavia Teixeira <flaviadteixeira@gmail.com>
2021-06-15 14:16:32 +0200

    added messages route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :labels, only: [:show, :new, :create, :edit, :update]
   resources :artists, only: [:show, :new, :create, :edit, :update]
-  resources :chatrooms, only: [:show]
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: [:create]
+  end
 end
