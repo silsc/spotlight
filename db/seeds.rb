a6b5af194a181192eeb9c6bdbea3759738daecaa
Flavia Teixeira <flaviadteixeira@gmail.com>
2021-06-17 17:48:59 +0200

    fixed the influences artist show

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,23 @@ Label.destroy_all
 
 puts "Creating artist influences"
 
-influences = [
-  {
-    name: 'Florence + The Machine', image: 'https://dynamicmedia.livenationinternational.com/Media/k/t/n/9cc3e38c-66b0-4160-8859-0feb42352add.jpg'
-  },
-  {
-    name: 'Phoebe Bridgers', image: 'https://media.resources.festicket.com/www/artists/phoebe-bridgers-cover-story-interview.jpg'
-  },
-  {
-    name: 'Erykah Badu', image:'https://www.clubbingspain.com/imagenes/Erykah-Badu-2015-1200.jpg'
-  },
-  {
-    name: 'Rage Against The Machine', image:'https://www.rollingstone.com/wp-content/uploads/2020/02/Rage.jpg?resize=1800,1200&w=1800'
-  },
-  {
-    name: 'Paramore', image:'https://indiehoy.com/wp-content/uploads/2018/09/paramore.jpg'
-  }
-]
+# influences = [
+#   {
+#     name: 'Florence + The Machine', image: 'https://dynamicmedia.livenationinternational.com/Media/k/t/n/9cc3e38c-66b0-4160-8859-0feb42352add.jpg'
+#   },
+#   {
+#     name: 'Phoebe Bridgers', image: 'https://media.resources.festicket.com/www/artists/phoebe-bridgers-cover-story-interview.jpg'
+#   },
+#   {
+#     name: 'Erykah Badu', image:'https://www.clubbingspain.com/imagenes/Erykah-Badu-2015-1200.jpg'
+#   },
+#   {
+#     name: 'Rage Against The Machine', image:'https://www.rollingstone.com/wp-content/uploads/2020/02/Rage.jpg?resize=1800,1200&w=1800'
+#   },
+#   {
+#     name: 'Paramore', image:'https://indiehoy.com/wp-content/uploads/2018/09/paramore.jpg'
+#   }
+# ]
 
 puts "Creating users, artist and label"
 
@@ -35,7 +35,7 @@ puts 'user-2 created'
 label = Label.create!(user: user_2, name: 'Sony', location: 'Brussels', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', website_url: 'www.sony.com')
 puts 'label Sony created'
 
-artist = Artist.create!(name: 'Biig Piig', age: 21, location: 'Dublin', user: user, label: label, genres: "Rock", influences: )
+artist = Artist.create!(name: 'Biig Piig', age: 21, location: 'Dublin', user: user, label: label, genres: "Rock")
 puts 'artist created'
 # Examples:
 #
