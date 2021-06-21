bb9bb8836188987cada8a943593eafdf6b335dd1
Iris Perisa <iris.perisa@gmail.com>
2021-06-21 14:45:17 +0200

    event route refactored

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :labels, only: [:show, :new, :create, :edit, :update] do
-    resources :events, only: [:new, :create], module: :labels
+    resources :events, only: [:new, :create]
   end
 
   resources :artists do
     resources :songs, only: [:new, :create]
-    resources :events, module: :artists
+    resources :events, only: [:new, :create]
   end
 
   resources :chatrooms, only: [:show] do
     resources :messages, only: [:create]
   end
 
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :events, only: [:index, :show]
-    end
-  end
   
   get '/styleguide', to: 'pages#styleguide'
   get '/redirect', to: 'pages#redirect'
