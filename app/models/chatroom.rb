c0017ffc9a91611dffd2b8f204d756d87016bab7
Silvia Salat <salat.silvia@gmail.com>
2021-06-22 12:28:06 +0200

    added other user name

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,17 @@ class Chatroom < ApplicationRecord
   has_many :conversations, dependent: :destroy
   has_many :messages, dependent: :destroy
   belongs_to :user
+
+  def get_other_user
+    user = conversations.first.user
+    if user.artist?
+      user.artist
+    else
+      user.label
+    end
+  end
+
+  def get_other_user_name
+    get_other_user.name
+  end
 end
