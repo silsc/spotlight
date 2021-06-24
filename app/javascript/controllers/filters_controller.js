af494881b3778687a635fe3c6069d944b3feed21
Carlos Villen <carlosvillen@gmail.com>
2021-06-24 16:36:16 +0100

    Added the function of submitting a form by checking the checkboxes.

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,15 +1,35 @@
 import { Controller } from "stimulus";
 
+function getUrlParams(search) {
+  const hashes = search.slice(search.indexOf('?') + 1).split('&')
+  const params = {}
+  hashes.map(hash => {
+      let [key, val] = hash.split('=')
+      key = decodeURIComponent(key)
+      if (key.indexOf('[') > -1) { // handle multiple type inputs
+          if (typeof params[key] === 'undefined') {
+              params[key] = []
+          }
+          params[key].push(decodeURIComponent(val))
+      } else {
+          params[key] = decodeURIComponent(val)
+      }
+  })
+  return params;
+}
+
 export default class extends Controller {
   static targets = [ 'form', 'checkbox' ];
   connect() {
-    // document.querySelector("#filter_genres_jazz").checked = true
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
+    const paramsGenres = getUrlParams(window.location.search)["filter[genres][]"];
+    const paramsRoles = getUrlParams(window.location.search)["filter[roles][]"];
 
-    // console.log(params["filter[genres][]"].includes('Rock'))
-
-    console.log(this.checkboxTargets)
+    this.checkboxTargets.forEach( (checkbox) => {
+      const labelText = (checkbox.nextElementSibling.innerText)
+      if ((paramsGenres && paramsGenres.includes(labelText)) || paramsRoles && paramsRoles.includes(labelText)) {
+        checkbox.checked = true;
+      }
+    })
   }
   filter(e) {
     this.formTarget.submit()
